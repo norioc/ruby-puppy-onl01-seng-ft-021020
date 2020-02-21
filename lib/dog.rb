6943cf550baf7f0f37699e1b1f25b8e9f27f364f
norio c <norionorioc@gmail.com>
2020-02-21 00:54:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
-# Add your code here
+class Dog
+  
+  attr_accessor :name
+  @@all = []
+  
+  def initialize(name)
+  @name = name 
+  end
+  
+  def self.all
+  @@all 
+  end
+  
+  def 
+  
+  
+  
+  
+  
+  
+  
+end
